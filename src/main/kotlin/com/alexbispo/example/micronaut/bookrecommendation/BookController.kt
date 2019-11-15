70d9dad6aff479cdd3092e9d39a1fc5e575a41fd
alexbispo <alexbispo.artes@gmail.com>
2019-11-15 05:24:53 -0300

    RPC funcionando.

--- src/main/kotlin/com/alexbispo/example/micronaut/bookrecommendation/BookController.kt
+++ src/main/kotlin/com/alexbispo/example/micronaut/bookrecommendation/BookController.kt
@@ -19,11 +19,11 @@ class BookController {
     fun index(): Flowable<BookRecommendation> {
         return catalogueClient.findAll(null)
                 .flatMap{ Flowable.fromIterable(it) }
-                .flatMapMaybe { book ->
-                    inventoryClient.stock(book.isbn)
-                            .filter{ it }
-                            .map { book }
-                }
-                .map { (_, name) -> BookRecommendation(name) }
+//                .flatMapMaybe { book ->
+//                    inventoryClient.stock(book.isbn)
+//                            .filter{ it is Boolean }
+//                            .map { book }
+//                }
+                .map { BookRecommendation(it.name) }
     }
 }
